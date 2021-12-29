892cdd8ddd98ab1825f174824235b5a840641647
Nejc Drobnič <ethoarx@gmail.com>
2021-12-29 18:53:56 +0100

    refactor: remove random logging

--- src/components/Connect/MetamaskConnect.tsx
+++ src/components/Connect/MetamaskConnect.tsx
@@ -1,12 +1,10 @@
 import React, { useEffect, useLayoutEffect, useRef, useState } from 'react';
 import MetaMaskOnboarding from '@metamask/onboarding';
-import { ActiveStatus } from 'utils/logs';
 import { useActiveWeb3React } from 'hooks/useActiveWeb3React';
 import { useSelector } from 'react-redux';
 import { selectEagerAttempt } from 'state/reducers/user';
 import { metamask } from 'connectors';
 import { UserRejectedRequestError } from '@web3-react/injected-connector';
-import Image from 'next/image';
 
 const MetamaskConnect: React.FC = () => {
 	const { active, error, activate, setError } = useActiveWeb3React();
@@ -24,7 +22,6 @@ const MetamaskConnect: React.FC = () => {
 			setConnecting(false);
 			onboarding.current?.stopOnboarding();
 		}
-		ActiveStatus(active);
 	}, [active, error]);
 
 	if (error || !triedToEagerConnect) {
@@ -33,29 +30,28 @@ const MetamaskConnect: React.FC = () => {
 
 	return (
 		<>
-			<button
-				className="w-full text-white p-2 m-4 bg-gray-400 border-4 border-gray-600 rounded-lg"
-				onClick={() => {
-					if (!MetaMaskOnboarding.isMetaMaskInstalled()) return onboarding.current?.startOnboarding();
-					setConnecting(true);
+			<div className="m-4">
+				<button
+					className="dark:text-gray-50 border dark:border-gray-600 dark:border-opacity-40 dark:bg-gray-800 transition-colors duration-300 ease-in-out dark:hover:bg-pink-900 dark:hover:border-pink-800 p-2 m-auto rounded-lg w-full"
+					onClick={() => {
+						if (!MetaMaskOnboarding.isMetaMaskInstalled()) return onboarding.current?.startOnboarding();
+						setConnecting(true);
 
-					activate(metamask, undefined, true).catch((error) => {
-						// ignore the error if it's a user rejected request
-						if (error instanceof UserRejectedRequestError) {
-							setConnecting(false);
-						} else {
-							setError(error);
-						}
-					});
-				}}
-			>
-				<div className="flex">
-					<span>{MetaMaskOnboarding.isMetaMaskInstalled() ? 'Metamask' : 'Install Metamask'}</span>
-					<div>
-						<Image src="/assets/wallets/metamask.png" height={32} width={32} loading="lazy" />
+						activate(metamask, undefined, true).catch((error) => {
+							// ignore the error if it's a user rejected request
+							if (error instanceof UserRejectedRequestError) {
+								setConnecting(false);
+							} else {
+								setError(error);
+							}
+						});
+					}}
+				>
+					<div className="flex">
+						<span>{MetaMaskOnboarding.isMetaMaskInstalled() ? 'Metamask' : 'Install Metamask'}</span>
 					</div>
-				</div>
-			</button>
+				</button>
+			</div>
 		</>
 	);
 };
