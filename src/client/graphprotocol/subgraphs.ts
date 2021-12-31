d830448a9280130144a8d1ef2811deaccad69f18
Nejc Drobnič <ethoarx@gmail.com>
2021-12-31 17:46:46 +0100

    feat: add remaining optimism subgraphs

--- src/client/graphprotocol/subgraphs.ts
+++ src/client/graphprotocol/subgraphs.ts
@@ -1,9 +1,9 @@
 export enum Subgraph {
-	MAINNET_EIP721 = 'https://api.thegraph.com/subgraphs/name/amxx/eip721-subgraph',
-	MAINNET_EIP1155 = 'https://api.thegraph.com/subgraphs/name/alexvorobiov/eip1155subgraph',
-	MAINNET_NON_STANDARD = 'https://thegraph.com/hosted-service/subgraph/0xorg/non-standard-nfts',
-	POLYGON_EIP721 = 'https://api.thegraph.com/subgraphs/name/quantumlyy/eip721-subgraph-matic',
-	POLYGON_EIP1155 = 'https://api.thegraph.com/subgraphs/name/quantumlyy/eip1155-subgraph-matic',
-	OPTIMISM_EIP721 = '',
-	OPTIMISM_EIP1155 = ''
+	MAINNET_EIP721 = 'https://api.thegraph.com/subgraphs/name/amxx/eip721-subgraph', // @subgraphs/eip721
+	MAINNET_EIP1155 = 'https://api.thegraph.com/subgraphs/name/alexvorobiov/eip1155subgraph', // @subgraphs/eip1155
+	MAINNET_NON_STANDARD = 'https://thegraph.com/hosted-service/subgraph/0xorg/non-standard-nfts', // @subgraphs/non-standard-nfts
+	POLYGON_EIP721 = 'https://api.thegraph.com/subgraphs/name/quantumlyy/eip721-subgraph-matic', // @subgraphs/eip721-matic or @subgraphs/eip721-optimism
+	POLYGON_EIP1155 = 'https://api.thegraph.com/subgraphs/name/quantumlyy/eip1155-subgraph-matic', // @subgraphs/eip1155-matic or @subgraphs/eip1155-optimism
+	OPTIMISM_EIP721 = 'https://api.thegraph.com/subgraphs/name/quantumlyy/eip721-subgraph-optimism', // @subgraphs/eip721-optimism or @subgraphs/eip721-matic
+	OPTIMISM_EIP1155 = 'https://api.thegraph.com/subgraphs/name/quantumlyy/eip1155-subgraph-optimism' // @subgraphs/eip1155-optimism or @subgraphs/eip1155-matic
 }
