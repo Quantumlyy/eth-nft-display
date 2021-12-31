f632e19c216e111725f8ff681c98d1c64a52066f
Nejc Drobnič <ethoarx@gmail.com>
2021-12-31 17:54:58 +0100

    fix: use api endpoint not browser for non standards

--- src/client/graphprotocol/subgraphs.ts
+++ src/client/graphprotocol/subgraphs.ts
@@ -1,7 +1,7 @@
 export enum Subgraph {
 	MAINNET_EIP721 = 'https://api.thegraph.com/subgraphs/name/amxx/eip721-subgraph', // @subgraphs/eip721
 	MAINNET_EIP1155 = 'https://api.thegraph.com/subgraphs/name/alexvorobiov/eip1155subgraph', // @subgraphs/eip1155
-	MAINNET_NON_STANDARD = 'https://thegraph.com/hosted-service/subgraph/0xorg/non-standard-nfts', // @subgraphs/non-standard-nfts
+	MAINNET_NON_STANDARD = 'https://api.thegraph.com/subgraphs/name/0xorg/non-standard-nfts', // @subgraphs/non-standard-nfts
 	POLYGON_EIP721 = 'https://api.thegraph.com/subgraphs/name/quantumlyy/eip721-subgraph-matic', // @subgraphs/eip721-matic or @subgraphs/eip721-optimism
 	POLYGON_EIP1155 = 'https://api.thegraph.com/subgraphs/name/quantumlyy/eip1155-subgraph-matic', // @subgraphs/eip1155-matic or @subgraphs/eip1155-optimism
 	OPTIMISM_EIP721 = 'https://api.thegraph.com/subgraphs/name/quantumlyy/eip721-subgraph-optimism', // @subgraphs/eip721-optimism or @subgraphs/eip721-matic
