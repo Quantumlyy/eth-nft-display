0c3fd3983f7da0a87a7187640e44f47d502f11d8
Nejc Drobnič <ethoarx@gmail.com>
2022-01-02 00:44:28 +0100

    feat: add arbitrum subgraphs to enum

--- src/client/graphprotocol/subgraphs.ts
+++ src/client/graphprotocol/subgraphs.ts
@@ -6,5 +6,7 @@ export enum Subgraph {
 	// POLYGON_EIP721 = 'https://api.thegraph.com/subgraphs/name/quantumlyy/eip721-subgraph-matic', // @subgraphs/eip721-matic or @subgraphs/eip721-optimism
 	POLYGON_EIP1155 = 'https://api.thegraph.com/subgraphs/name/quantumlyy/eip1155-subgraph-matic', // @subgraphs/eip1155-matic or @subgraphs/eip1155-optimism
 	OPTIMISM_EIP721 = 'https://api.thegraph.com/subgraphs/name/quantumlyy/eip721-subgraph-optimism', // @subgraphs/eip721-optimism or @subgraphs/eip721-matic
-	OPTIMISM_EIP1155 = 'https://api.thegraph.com/subgraphs/name/quantumlyy/eip1155-subgraph-optimism' // @subgraphs/eip1155-optimism or @subgraphs/eip1155-matic
+	OPTIMISM_EIP1155 = 'https://api.thegraph.com/subgraphs/name/quantumlyy/eip1155-subgraph-optimism', // @subgraphs/eip1155-optimism or @subgraphs/eip1155-matic
+	ARBITRUM_EIP721 = 'https://api.thegraph.com/subgraphs/name/quantumlyy/eip721-subgraph-arbitrum',
+	ARBITRUM_EIP1155 = 'https://api.thegraph.com/subgraphs/name/quantumlyy/eip1155-subgraph-arbitrum'
 }
